ef2dc2b09bea646b1f4ba3588088172bf7dbcd1f
uelkariuki <cratzuel@gmail.com>
2023-02-16 18:50:05 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-int puts(\"Programming is like building a multilingual puzzle);
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return : Always 0 (Success)
+  */
+int main(void)
+{
+
+/* initializing strings */
+char str1 [] = "/""Programming is like building a multilingual puzzle";
+
+/* writing to stdout */
+puts(str1);
+
+	return (0);
+}

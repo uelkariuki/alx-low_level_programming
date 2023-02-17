f58124979262de9d5cafa16dabaf6e87002bda8d
uelkariuki <cratzuel@gmail.com>
2023-02-17 22:37:19 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  *main -entry point
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; i++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+
+
+			}
+
+
+		}
+
+	}
+	return (0);
+
+
+}

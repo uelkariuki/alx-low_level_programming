f07fdf2ae5b857f566a8a54cb1a3aec4425b862c
uelkariuki <cratzuel@gmail.com>
2023-02-17 00:19:13 +0000

    task 8 updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
   *main - Entry
@@ -8,7 +7,7 @@
   */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }

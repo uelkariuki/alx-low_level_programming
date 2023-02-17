619c0b5e31d2f4b9f9550709fc76fc4e8eb2c35d
uelkariuki <cratzuel@gmail.com>
2023-02-17 20:42:55 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main- entry point
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i;
+	/* use a for loop to iterate from values 0 to 9) */
+	for (i = 0; i < 10; i++)
+	{ putchar(i + '0');
+
+	}
+	putchar('\n');
+	return (0);
+
+}

66942476aadb1438815a5a418297b40ca3b5e2d1
uelkariuki <cratzuel@gmail.com>
2023-02-17 23:17:46 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,32 +2,28 @@
 
 /**
   *main -entry point
-  *
+  *Description: program that prints all possible
+  *		combinations of single-digit numbers.
   *Return: 0
   */
 
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		putchar(i + '0');
+
+		if (i < 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i < 8)
-			{
-				putchar(',');
-				putchar(' ');
-
-
-			}
-
-
+			putchar(',');
+			putchar(' ');
 		}
 
 	}
+
+	putchar('\n');
 	return (0);
 
 

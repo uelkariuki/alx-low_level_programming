bb1873dd4a2c2c7c83c72c5571ba3394a61d2853
uelkariuki <cratzuel@gmail.com>
2023-02-17 22:52:23 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,13 +5,14 @@
   *
   *Return: 0
   */
+
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 10; i++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');

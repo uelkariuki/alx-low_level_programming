da8d9f5006cc777c8b3ded3939da6e38cffc1a72
uelkariuki <cratzuel@gmail.com>
2023-02-19 20:59:06 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  *main -  prints all possible different combinations of two digits
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	/* initialize digits */
+	int i, j;
+
+	/* use a for loop to add first digit */
+	for (i = 0; i < 10; i++)
+	{
+		/* use a for loop to add second digit */
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			/* to input commas and space after digits */
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+
+
+		}
+
+
+
+	}
+	return (0);
+
+}

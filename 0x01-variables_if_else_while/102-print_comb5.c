c109fd6a2467a93eec558a2ae7129360a211c26c
uelkariuki <cratzuel@gmail.com>
2023-02-20 00:17:45 +0000

    task 12 and updated README.md file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,18 +5,22 @@
   */
 int main(void)
 {
+	/* initialize / declare  digits */
 	int i, j;
 	{
+		/* for loop for the two two- digit numbers */
 		for (i = 0; i < 100; i++)
 		{
 			for (j = i + 1; j < 100; j++)
 			{
+				/* use division: first digit, modulus: last digit*/
 				putchar(i / 10 + '0');
 				putchar(i % 10 + '0');
 				putchar(' ');
 				putchar(j / 10 + '0');
 				putchar(j % 10 + '0');
 
+				/* add commas and spaces*/
 				if (i != 98 || j != 99)
 				{
 					putchar(',');

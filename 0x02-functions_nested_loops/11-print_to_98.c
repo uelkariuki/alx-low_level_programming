0b220d0d1dae945d32940deaaac684d1d48e5161
uelkariuki <cratzuel@gmail.com>
2023-02-22 02:31:23 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,8 +13,8 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-	for (i = n; i <= 98; i++)
-	{
+		for (i = n; i <= 98; i++)
+		{
 		_putchar(i / 10 + '0');
 		_putchar(i % 10 + '0');
 		if (i != 98)
@@ -22,7 +22,7 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
-	}
+		}
 	}
 	else
 	{

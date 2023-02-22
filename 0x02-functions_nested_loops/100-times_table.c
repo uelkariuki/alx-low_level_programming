e314c8a75e0cad9459d7ed72ee8f22f0517447b8
uelkariuki <cratzuel@gmail.com>
2023-02-22 14:26:22 +0000

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,21 +21,32 @@ void print_times_table(int n)
 		for (j = 0; j < n; j++)
 		{
 			result = i * j;
+			if (j == 0)
 			{
+				_putchar('0');
+			}
 				if (result < 10)
 				{
-					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result > 100)
+				{
+					_putchar(result / 100 + '0');
+					_putchar(result / 10 % 10 + '0');
+					_putchar(result % 10);
 				}
 				else
 				{
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
+				}
+				if (j != n)
+				{
 					_putchar(',');
 					_putchar(' ');
 				}
-			}
 		}
 	}
-	_putchar('\n');
 }

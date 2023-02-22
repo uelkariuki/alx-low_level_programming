4108180baad97f4ab4c496fe4cff4578a1da8d94
uelkariuki <cratzuel@gmail.com>
2023-02-22 16:20:23 +0000

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,41 @@
 
 void print_times_table(int n)
 {
-	int i, j, k;
+	int x, y, z;
 
 	if (n >= 0 && n <= 14)
 	{
-		for (i = 0; i <= n; i++)
+		for (x = 0; x <= n; x++)
 		{
-			for (j = 0; j <= n; j++)
+			for (y = 0; y <= n; y++)
 			{
-				k = i * j;
-				if (k > 99)
+				z = x * y;
+				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if (k > 9)
+				else if (z > 9)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(((k / 10) % 10)  + '0');
-					_putchar((k % 10) + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					if (j != n)
+					if (y != 0)
 					{
 						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
 					}
-					_putchar(k + '0');
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');

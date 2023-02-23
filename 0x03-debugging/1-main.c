c94f4f31483f9e4c455a8bd42bfc8e9a24852852
uelkariuki <cratzuel@gmail.com>
2023-02-23 05:04:32 +0000

    infite loop removed \o/

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,10 @@ int main(void)
 
         i = 0;
 
-       /*comment out this area causes infinite loop
-	while (i < 10)
-	{
-                putchar(i);
-        }*/
+	/*while (i < 10)*/
+	/*{*/
+               /* putchar(i);*/
+       /* }*/
 
         printf("Infinite loop avoided! \\o/\n");
 

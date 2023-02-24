ee5fc70c1a85933def7de8c850ec470edb4c16a1
uelkariuki <cratzuel@gmail.com>
2023-02-24 00:46:38 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,27 @@ int main(void)
 
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+
+		}
+		if (i != 100)
+		{
+			printf(" ");
 
 		}
 

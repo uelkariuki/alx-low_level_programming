0bb7c31bab0ef0a489d4657f1f01ca814c5013f5
uelkariuki <cratzuel@gmail.com>
2023-02-27 19:53:58 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,18 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
+	{
+		j++;
+
+	}
+
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 
 	}
 	_putchar('\n');

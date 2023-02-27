59152aa5cd7a7f1fadc2f7589a356f1f23b83686
uelkariuki <cratzuel@gmail.com>
2023-02-27 11:30:21 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *swap_int- function that swaps the values of two integers.
+  *temp: temporary local variable
+  *@a: integer to be swapped
+  *@b: integer to be swapped
+  */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+
+
+
+}

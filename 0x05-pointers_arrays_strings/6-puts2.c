6ec89765929893cd42531f753dd20e2a8210e768
uelkariuki <cratzuel@gmail.com>
2023-02-27 18:54:30 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,6 +4,7 @@
   *puts2- function that prints every other character of a string, starting
   *with the first character, followed by a new line.
   *@str:string
+  *Return: voidd
   */
 
 void puts2(char *str)

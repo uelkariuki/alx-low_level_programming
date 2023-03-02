a8b1d8de2513881e1db6696bb3e307a5052db896
uelkariuki <cratzuel@gmail.com>
2023-03-02 22:51:21 +0000

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,12 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2, i, digit1, digit2, maxLen, carry = 0, sum = 0;
+	/* Find the lengths of the input numbers*/
+	int len1, idx2, idx1, len2, carry = 0, idxr = 0, digit, digit1;
+	int digit2, i, j, sum;
+	char temp;
+
+	len1 = 0;
 
 	while (n1[len1] != '\0')
 	{
@@ -23,33 +28,50 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		len2++;
 	}
-	maxLen = len1 > len2 ? len1 : len2;
+	/* Add the numbers digit by digit, starting from the least significant digit*/
+	carry = 0, idx1 = len1 - 1, idx2 = len2 - 1, idxr = 0;
 
-	if (maxLen + 1 > size_r)
-	{ /* check if result can be stored in r*/
-		return (0);
-	}
-
-	for (i = 0; i < maxLen; i++)
+	while (idx1 >= 0 || idx2 >= 0)
 	{
-		digit1 = i < len1 ? n1[len1 - 1 - i] - '0' : 0; /* get digit from n1*/
-		digit2 = i < len2 ? n2[len2 - 1 - i] - '0' : 0; /* get digit from n2*/
+		digit1 = (idx1 >= 0) ? (n1[idx1] - '0') : 0;
+		digit2 = (idx2 >= 0) ? (n2[idx2] - '0') : 0;
 		sum = digit1 + digit2 + carry;
 		carry = sum / 10;
-		sum %= 10;
-		r[maxLen - 1 - i] = sum + '0';
-	}
-	if (carry)
-	{
-		if (maxLen + 1 > size_r)
-		{ /* check if result can be stored in r*/
-			return (0);
+		digit = sum % 10;
+		if (idxr < size_r - 1)
+		{
+			r[idxr] = digit + '0';
+			idxr++;
 		}
-		r[0] = carry + '0';
-		r[maxLen] = '\0';
-		return (r);
+		else
+		{
+			return (0);  /* Result cannot be stored in r*/
+		}
+		idx1--;
+		idx2--;
 	}
-	r[maxLen] = '\0';
-	return (r);
-}
 
+	if (carry > 0 && idxr < size_r - 1) /* Handle any remaining carry*/
+	{
+		r[idxr] = carry + '0';
+		idxr++;
+	}
+	else if (carry > 0)
+	{
+		return (0); /* Result cannot be stored in r*/
+	}
+
+	r[idxr] = '\0'; /* Add a null terminator to the result string*/
+
+	i = 0, j = idxr - 1; /* Reverse the result string*/
+
+	while (i < j)
+	{
+		temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+		i++;
+		j--;
+	}
+	return (r); /*Return a pointer to the result string*/
+}

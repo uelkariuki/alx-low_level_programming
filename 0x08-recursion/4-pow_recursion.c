cb7b9fe7edd0735897de8f31fc4bc3bc97c91e1f
uelkariuki <cratzuel@gmail.com>
2023-03-08 16:29:23 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * pow(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 
 
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <math.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);

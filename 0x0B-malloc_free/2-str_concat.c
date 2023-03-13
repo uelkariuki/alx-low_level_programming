3ced52a839ee014ab3158dc3d68347029f49e819
uelkariuki <cratzuel@gmail.com>
2023-03-13 17:31:42 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,10 +21,15 @@ char *str_concat(char *s1, char *s2)
 	size_t s1_len;
 	size_t s2_len;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
+
 
 	}
 

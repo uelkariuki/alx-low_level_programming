1051f0776c9d6948f8ce40a5798f7eb2713390fb
uelkariuki <cratzuel@gmail.com>
2023-03-16 17:34:15 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,13 +2,8 @@
 
 /**
   *_calloc- function that allocates memory for an array, using malloc.
-<<<<<<< HEAD
-  *@nmemb: number of elements
-  *@size: size of the array
-=======
   *@nmemb: number of elements in the array
   *@size: size of the each element in the array
->>>>>>> 1125b0972ebf6d2b438b136d7954cf18a2c93d02
   *Return: pointer to the allocated memory
   */
 
@@ -32,8 +27,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	memset(ptr, 0, nmemb * size);
 	return (ptr);
-<<<<<<< HEAD
-
-=======
->>>>>>> 1125b0972ebf6d2b438b136d7954cf18a2c93d02
 }

6e4826a1b3c0df222024168fffedc7fc0f647bbd
uelkariuki <cratzuel@gmail.com>
2023-03-16 15:00:15 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = ("");
+		s2 = "";
 	}
 
 	if (n >= s2_len)

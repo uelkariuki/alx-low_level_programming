e78de6f9a60ef5d4cf65def77a805aedf81aec71
uelkariuki <cratzuel@gmail.com>
2023-03-16 15:50:23 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,8 +3,7 @@
 
 
 /**
-  *string_nconcat- function that concatenates two strings.
-  *
+  *string_nconcat- function that concatenates two strings.*
   *@s1: destination string to be concanated
   *@s2: source string two to be concanated
   *@n: number of bytes to be copied for the concantnated string
@@ -14,7 +13,6 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-
 	unsigned int s1_len = strlen(s1);
 	unsigned int s2_len = strlen(s2);
 
@@ -26,6 +24,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
 
 	if (n >= s2_len) /* Truncate s2 if necessary */
 	{
@@ -35,19 +38,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str = malloc(s1_len + 1);
 		if (str == NULL)
-		{
 			return (NULL);
-
-		}
 		memcpy(str, s1,  s1_len + 1);
 		return (str);
 	}
 
 	str = malloc(s1_len + n  + 1);
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 	memcpy(str, s1, s1_len);
 	memcpy(str + s1_len, s2, n);
 	str[s1_len + n] = '\0';

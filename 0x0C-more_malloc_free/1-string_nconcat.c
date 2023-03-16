b2cecaabacc57406370340fd85f7dbeed7d9eea3
uelkariuki <cratzuel@gmail.com>
2023-03-16 14:50:30 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = s2_len;
 	}
 
-	str = (char *) malloc(sizeof(char *) * (s1_len + n + 1));
+	str = malloc(s1_len + n + 1);
 
 	if (str == NULL)
 	{

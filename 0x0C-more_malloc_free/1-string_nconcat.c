baa10c379152b35970d5585055705262be79ef7f
uelkariuki <cratzuel@gmail.com>
2023-03-16 14:38:21 +0000

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+  *string_nconcat- function that concatenates two strings.
+  *
+  *@s1: destination string to be concanated
+  *@s2: source string two to be concanated
+  *@n: number of bytes to be copied for the concantnated string
+  *Return: pointer having the concantenated string
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	size_t s1_len, s2_len;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+
+	str = (char *) malloc(sizeof(char *) * (s1_len + s2_len + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (n >= strlen(s2))
+	{
+		s2++;
+	}
+
+	memcpy(str, s1, n);
+	memcpy(str + s1_len, s2, n);
+
+	str[s1_len + s2_len] = '\0';
+	return (str);
+
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,7 +1,8 @@
 # 0x0C-more_malloc_free
 ## Tasks
 
-
+0. Write a function that allocates memory using malloc.
+1. Write a function that concatenates two strings.
 
 
 

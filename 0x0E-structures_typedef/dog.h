710e07d95ccdfbf51dc6683efd2278e84df63791
uelkariuki <cratzuel@gmail.com>
2023-03-20 17:58:04 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+  *struct dog- new type struct dog with the following element
+  *@name: the name
+  *@age: the age
+  *@owner: the owners
+  */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

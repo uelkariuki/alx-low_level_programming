eb4495fda2bab0dce46015efe622952a85514ce9
uelkariuki <cratzuel@gmail.com>
2023-03-20 16:23:09 +0000

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+
+/**
+  *function-like macro ABS(x) that computes the absolute value of a number x.
+  *@x: the number to find the absolute value
+  */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO.H*/

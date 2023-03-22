0c8e19eeaddea3658f3b3c780f4940152a258972
uelkariuki <cratzuel@gmail.com>
2023-03-22 16:11:17 +0000

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,26 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL)
 	{
-		action(array[i]);
+		return;
+
+	}
+	if (size <= 0)
+	{
+		return;
+	}
+	if (action == NULL)
+	{
+		return;
+
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 
 }

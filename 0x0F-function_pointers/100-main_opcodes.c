dc021a7d0518d1c282c0e9cbc2964f54f473ce6a
uelkariuki <cratzuel@gmail.com>
2023-03-23 00:55:53 +0000

    task 4 #advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main- program that prints the opcodes of its own main function.
+  *@argc: the number of command line arguments
+  *@argv: array containing the program commandline arguments
+  *Return: the opcodes(machine code) of main function
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, number_of_bytes;
+	char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (char *)&main;
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%02hhx ", ptr[i]);
+
+	}
+	printf("\n");
+	return (0);
+
+}
+

0c5cea6fd3fd2386d726cef6713fc531acaae77a
uelkariuki <cratzuel@gmail.com>
2023-04-04 23:30:41 +0000

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,6 +10,12 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current_node = head;
 	size_t counter = 0;
 
+	if (head == NULL)
+	{
+		exit(98);
+
+	}
+
 	while (current_node != NULL)
 	{
 		printf("[%p] %d\n", (void *)current_node, current_node->n);

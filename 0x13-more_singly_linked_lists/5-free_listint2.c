a19c05531bb5f49a096491003d06bf39a0345673
uelkariuki <cratzuel@gmail.com>
2023-04-04 01:16:12 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	listint_t *current_node = *head;
 	listint_t *temp;
 
-	if (head == NULL)/* here it checks if head pointer is null*/
+	if (*head == NULL)/* here it checks if head pointer is null*/
 	{
 		return;
 	}

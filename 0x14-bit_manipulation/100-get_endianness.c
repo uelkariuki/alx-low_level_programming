d407e79ee88aa5e0a21fdc0064946b66c7152f57
uelkariuki <cratzuel@gmail.com>
2023-04-08 02:40:56 +0000

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -9,6 +9,9 @@
 int get_endianness(void)
 {
 	unsigned int store_int = 0x23456789;
+
+	/*type casting  using (char *) to access*/
+	/*the individual bytes as characters to check the byte order*/
 	char *point_int = (char *) &store_int;
 
 	if (*point_int == 0x23)

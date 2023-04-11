dd6039971f91c3c698be9dc666d40deaa15f2d7b
uelkariuki <cratzuel@gmail.com>
2023-04-11 00:54:10 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,65 +2,82 @@
 
 #define BUFFER_SIZE 1024
 
-
 /**
-  *main- programthat copies the content of a file to another file.
-  *@argc: the number of commandline arguments
-  *@argv: the array containing the program command line arguments
-  *Return: (0);
+  *error_handling- function to handle the incoming errors and print it
+  *to the stderr
+  *@exit_code: the error code to exit using
+  *@error_message: the error message to be printed
   */
 
-int main(int argc, char *argv[])
+void error_handling(int exit_code, const char *error_message)
+{
+	dprintf(STDERR_FILENO, "%s", error_message);
+	exit(exit_code);
+}
+/**
+  *copy_file_content- function that copies content of a file to another file.
+  *@filename_from: path of file to read from
+  *@filename_to: path of file to write to
+  */
+
+void copy_file_content(const char *filename_from, const char *filename_to)
 {
 	int file_from, file_to;
 	char text_content_buffer[BUFFER_SIZE];
 	ssize_t t_bytes_read, t_bytes_written;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
+
+	file_from = open(filename_from, O_RDONLY);
 	if (file_from == -1)/* not exist and cant be opened*/
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]);
-		exit(98);
+		error_handling(98, "Error: Can't read from file\n");
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+	file_to = open(filename_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		error_handling(99, "Error: Can't write to file\n");
 		close(file_from);
-		exit(99);
 	}
 	do {
-		t_bytes_read = read(file_from, text_content_buffer,BUFFER_SIZE);
+		t_bytes_read = read(file_from, text_content_buffer, BUFFER_SIZE);
 		if (t_bytes_read == -1)
 		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+			error_handling(98, "Error: Can't read from file \n");
 			close(file_from);
 			close(file_to);
-			exit(98);
 		}
 		t_bytes_written = write(file_to, text_content_buffer, t_bytes_read);
 		if (t_bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-					argv[2]);
+			error_handling(99, "Error: Can't write to file\n");
 			close(file_from);
 			close(file_to);
-			exit(99);
 		}
-	}while (t_bytes_read > 0);
-
-	if (close(file_from) == -1 || close(file_to) == -1) 
+	} while (t_bytes_read > 0);
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
+		error_handling(100, "Error: Can't close fd\n");
 	}
+}
+
+
+/**
+  *main- program that copies the content of a file to another file.
+  *@argc: the number of commandline arguments
+  *@argv: the array containing the program command line arguments
+  *Return: 0 if succesful, 1 if not
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		error_handling(97, "Usage: cp file_from file_to\n");
+
+	}
+
+	copy_file_content(argv[1], argv[2]);
 	return (0);
 }
+

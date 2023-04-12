3244254b463c6ed8797d414e4d1d657f95614e1f
uelkariuki <cratzuel@gmail.com>
2023-04-12 02:04:33 +0000

    task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,21 +16,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-/*typedef struct {
-	unsigned char e_indent[EI_NIDENT];
-	uint16_t  e_type;
-	uint16_t e_machine;
-	uint32_t e_version;
-	uint64_t e_entry;
-	uint64_t e_phoff;
-	uint64_t e_shoff;
-	uint32_t e_flags;
-	uint16_t e_ehsize;
-	uint16_t e_phentsize;
-	uint16_t e_phnum;
-	uint16_t e_shentsize;
-	uint16_t e_shnum;
-	uint16_t e_shstrndx;
-} fghfghfghgfhfghfgElfN_Ehdr;*/
-
 #endif

7e0c80c7e076ef1e4200a9c0993d62b6798066eb
uelkariuki <cratzuel@gmail.com>
2023-04-15 03:27:55 +0000

    update README.md

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,16 +10,10 @@
 #include <stdint.h>
 #include <elf.h>
 
-/* #definehgfhdfghfhfgdhdfh EI_NIDENT 16*/
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
- #if __LP64__
-typedef Elf64_Ehdr Elf_Ehdr;
-#else
-typedef Elf32_Ehdr Elf_Ehdr;
-#endif
 
 #endif

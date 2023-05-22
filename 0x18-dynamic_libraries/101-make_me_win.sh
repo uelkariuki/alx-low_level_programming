6a20cd5923703e96c5fc1e8c8294f1e07117b5d9
uelkariuki <cratzuel@gmail.com>
2023-05-22 20:33:39 +0000

    Win Giga Millions Jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp 0x18-dynamic_libraries/giga_win_lib.so
+wget -P /tmp https://github.com/uelkariuki/alx-low_level_programming/blob/master/0x18-dynamic_libraries/giga_win_lib.so
 export LD_PRELOAD=/tmp/giga_win_lib.so

e94f72e6c354526316617d18400dba0b1df3fed7
uelkariuki <cratzuel@gmail.com>
2023-06-09 15:46:18 +0000

    Task 3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set- function that adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key and key can not be an empty string
+ * @value: is the value associated with the key
+ * value must be duplicated, value can be an empty string
+ * In case of collision add the new node at the beginning of the list
+ * Return: 1 if it succeeded, 0 otherwise
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *temp, *the_new_node;
+
+	index = key_index((unsigned char *)key, ht->size);
+	if (index == 0)
+		return (0);
+
+	temp = ht->array[index];
+
+	while (temp != NULL)
+	{
+		/*the key is found*/
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (0);
+		}
+		temp = temp->next;
+	}
+
+	/* Because of collision, add new_node at start of the list*/
+	the_new_node = (hash_node_t *)malloc(sizeof(hash_table_t));
+	if (the_new_node == NULL)
+	{
+		return (0);
+	}
+	the_new_node->key = strdup(key);
+	the_new_node->value = strdup(value);
+
+	the_new_node->next = ht->array[index];
+	ht->array[index] = the_new_node;
+
+	return (1);
+
+}

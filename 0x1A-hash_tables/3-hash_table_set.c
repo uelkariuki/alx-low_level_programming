e5957bd11ecda226b4755ca59bd9bc77466592cf
uelkariuki <cratzuel@gmail.com>
2023-06-09 16:23:11 +0000

    Task 3.4 >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,11 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *temp, *the_new_node;
 
-	if (value == NULL)
-		return (0);
-	if (ht == NULL)
-		return (0);
-	if (key == NULL)
+	if (value == NULL || ht == NULL || key == NULL || *key == '\0' ) 
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (index == 0)

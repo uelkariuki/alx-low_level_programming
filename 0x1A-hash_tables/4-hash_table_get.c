b75f9bf1c4f7313783c27833a4af3decca0ba548
uelkariuki <cratzuel@gmail.com>
2023-06-09 17:51:58 +0000

    Task 4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get- a function that retrieves a value associated with a key.
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *item;
+	unsigned long int index;
+
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	index = key_index((unsigned char *) key, ht->size);
+	item = ht->array[index];
+
+	while (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+		{
+			return (item->value);
+
+		}
+
+	}
+	return (NULL);
+
+}

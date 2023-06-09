9ef401d248674a2c908a2eb99c23e2e5512ee6c6
uelkariuki <cratzuel@gmail.com>
2023-06-09 08:37:49 +0000

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create- function that creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int q;
+
+	if (size == 0)
+		return (0);
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (q = 0; q < size; q++)
+	{
+		table->array[q] = NULL;
+
+
+	}
+	return (table);
+
+
+
+
+
+}

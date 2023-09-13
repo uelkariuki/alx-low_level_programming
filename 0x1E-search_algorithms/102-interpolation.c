f330ca2816c7331641c11e1fe981a1421ced7671
uelkariuki <cratzuel@gmail.com>
2023-09-13 13:10:04 +0000

    Function that searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,13 +12,11 @@
 
 int helper_interpolation(int *array, int low, int high, int value)
 {
-	size_t pos = 2109;
+	size_t pos = low + (((double)(high - low) /
+				(array[high] - array[low])) * (value - array[low]));
 
 	if (low <= high && value >= array[low] && value <= array[high])
 	{
-
-		pos = low + (((double)(high - low) /
-					(array[high] - array[low])) * (value - array[low]));
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
